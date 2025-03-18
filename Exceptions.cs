3677e553b0d3d90657c29fbfb8a4221c4845c8b1
iwrawww@gmail.com <iwrawww@gmail.com>
2025-03-18 08:44:59 +0700

    исключения

--- Exceptions.cs
+++ Exceptions.cs
@@ -2,10 +2,10 @@
 
 public class MatrixSizeMismatchException : Exception
 {
-    public MatrixSizeMismatchException(string message) : base(message) { }
+  public MatrixSizeMismatchException(string message) : base(message) { }
 }
 
 public class InvalidMatrixOperationException : Exception
 {
-    public InvalidMatrixOperationException(string message) : base(message) { }
+  public InvalidMatrixOperationException(string message) : base(message) { }
 }
